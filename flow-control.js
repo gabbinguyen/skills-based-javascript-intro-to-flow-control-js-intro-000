251ea5b6aad2d5c38818022585121e14834ae67e
magical-builder-5600 <gabbinguyen@gmail.com>
2020-08-01 05:00:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
 function basicTeenager(age) {
-  if (age >= 13 && <= 19 ) {return = "You are a teenager!";}
+  if (age >= 13 && <= 19 ) {return "You are a teenager!";}
 }
 
 function teenager(age) {
   if (age >= 13 && <= 19 ) {return = "You are a teenager!";}
-else {return="You are not a teenager"}
+else {return = "You are not a teenager"}
 }
 
 function ageChecker(age) {
@@ -14,7 +14,8 @@ else {return = "You are a grownup";}
 }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && <= 19 ) {return = "You are a teenager!";}
+else {return = "You are not a teenager"}
 }
 
 function switchAge(age) {
